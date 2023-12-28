962f6b6de0c1f0439974a31bc3f656c667d3cc9b
EliMundo <elibekmd@gmail.com>
2023-12-28 07:38:01 +0600

    Remaking since there was an issue

--- restapp/meal_app/urls.py
+++ restapp/meal_app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from meal_app import views
+from django.urls import  path
+from . import views
 
 urlpatterns = [
-    path('', views.get_meals, name="get_meals"),
+    path('', views.get_meals, name = "get_meals")
 ]
